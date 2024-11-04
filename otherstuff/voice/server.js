ef01be1d90c053b5d6184a4e1ba62c74ac55ec99
Aether <luke.b.hartung@outlook.com>
2024-11-04 17:06:16 -0500

    i gave up

--- otherstuff/voice/server.js
+++ otherstuff/voice/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const app = express();
 
 // Serve static files from 'public' folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Create HTTP server
 const server = http.createServer(app);
@@ -15,23 +15,23 @@ const wss = new WebSocket.Server({ server });
 // Store connected clients
 let clients = [];
 
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
-  clients.push(ws);
-  ws.on('message', function incoming(message) {
-    // Broadcast messages to other clients
-    clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
+wss.on("connection", function connection(ws) {
+    console.log("Client connected");
+    clients.push(ws);
+    ws.on("message", function incoming(message) {
+        // Broadcast messages to other clients
+        clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
     });
-  });
-  ws.on('close', function() {
-    clients = clients.filter(client => client !== ws);
-  });
+    ws.on("close", function () {
+        clients = clients.filter((client) => client !== ws);
+    });
 });
 
 // Start the server on port 3000
 server.listen(3001, function listening() {
-  console.log('Server is listening on port 3000');
+    console.log("Server is listening on port 3000");
 });
