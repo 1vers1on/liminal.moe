a5219ef2dc170dc32e0b83a0c611c87ce22f871d
1vers1on <elliehartung@outlook.com>
2025-04-21 08:13:09 -0400

    oops i was a dumbass mb

--- src/routes/api/getSigmacoins/+server.ts
+++ src/routes/api/getSigmacoins/+server.ts
@@ -1,4 +1,4 @@
-/* Copyright (C) 2025 Eleanor Hartung
+/* Copyright (C) 2025 Ellie Hartung
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
